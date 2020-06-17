209164f326b167f2b8c8cc10b2ea827c64adff7d
marcoscz1995 <marcos.cardenas-zelaya@mail.mcgill.ca>
2020-06-17 17:23:29 -0400

    removed files that used evdev and hence no longer needed.

--- keyboard_event_determiner.py
+++
@@ -1,8 +0,0 @@
-from subprocess import run, TimeoutExpired
-
-try:
-    run(["python", "event_namer.py"], check=True, timeout=3)
-    print("scraped :)", "event_namer.py")
-except TimeoutExpired:
-    message_timeout = "Write down the number you see above. That is your keyboard event ID"
-    print(message_timeout)
